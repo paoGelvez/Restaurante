6a78c39964168d829a494b78de87438968a65272
dRicardo78 <dricardo7.8@hotmail.com>
2026-04-28 01:28:44 -0500

    Se actualiza server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/api/test', (req, res) => {
 });
 
 // Puerto
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor ejecutándose en puerto ${PORT}`);
 });
